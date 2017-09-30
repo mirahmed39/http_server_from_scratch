088c5cbb69b25959da6febc36bdc9bf607977299
Mir Ahmed <mir.ahmed39@yahoo.com>
2017-09-30 17:29:03 -0400

    implement part 1

--- src/warmUp.js
+++ src/warmUp.js
@@ -1,1 +1,13 @@
 // warmUp.js
+const net = require('net');
+const HOST = '127.0.0.1';
+const PORT = 8080;
+
+const server = net.createServer((sock) => {
+    sock.on('data', function (data) {
+        console.log("data recieved");
+        sock.write('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<em>Hello</em> <strong>World</strong>');
+        sock.end();
+    });
+});
+server.listen(PORT);
